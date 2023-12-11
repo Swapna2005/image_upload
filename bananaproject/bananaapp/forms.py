4d27b4c6495d0aca86a0e6c4ebcae95af91bfc0a
Swapna2005 <146418921+Swapna2005@users.noreply.github.com>
2023-12-11 11:10:28 +0530

    Add files via upload

---
+++ bananaproject/bananaapp/forms.py
@@ -0,0 +1,12 @@
+
+
+# forms.py
+from django import forms
+from .models import Hotel
+ 
+ 
+class HotelForm(forms.ModelForm):
+ 
+    class Meta:
+        model = Hotel
+        fields = ['name', 'hotel_Main_Img']
